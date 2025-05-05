3765402b108e2a00e08235bf8df34ef5afb0fa1d
COMLAB507 <COMLAB507@DESKTOP-2L2F8UA>
2025-05-05 11:28:47 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TeamAscend.Agapay
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamAscend.Agapay
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
