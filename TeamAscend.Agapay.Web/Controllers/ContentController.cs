8ad02d8ab40d9ffc13b70f62740351c703db3328
Erwin Mendoza <khronosangel@gmail.com>
2025-10-09 05:05:23 +0800

    [Update] Blog Post added

--- TeamAscend.Agapay.Web/Controllers/ContentController.cs
+++ TeamAscend.Agapay.Web/Controllers/ContentController.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using TeamAscend.Agapay.Web.Attributes;
 using TeamAscend.Agapay.Web.Models;
 using TeamAscend.Agapay.Web.Shared;
@@ -12,23 +11,87 @@ namespace TeamAscend.Agapay.Web.Controllers
         [Route("~/Admin/Announcements")]
         public IActionResult Announcements()
         {
-            return View("Index");
+            var resp = new List<BlogPost>();
+
+            using (AgapayTestDBContext db = new AgapayTestDBContext())
+            {
+                var posts = (from row in db.BlogPosts where !row.IsDeleted select row).ToList();
+                if (posts != null)
+                {
+                    resp = posts;
+                }
+            }
+            return View("Index",resp);
         }
-        
+
         [Route("~/Admin/InfoGraphs")]
         public IActionResult InfoGraphs()
         {
             return View("Index");
         }
 
-        public IActionResult BlogPosts()
+        [HttpGet]
+        public BlogPost GetBlogPost(int ID)
         {
-            return View("Index");
+            BlogPost resp = null;
+
+            using (AgapayTestDBContext db = new AgapayTestDBContext())
+            {
+                resp = (from row in db.BlogPosts where row.ID == ID && !row.IsDeleted select row).FirstOrDefault();
+            }
+
+            return resp;
         }
 
-        public IActionResult NewsArticles()
+        [HttpGet]
+        public int DeleteBlogPost(int ID)
         {
-            return View("Index");
+            int res = 0;
+
+            using (AgapayTestDBContext db = new AgapayTestDBContext())
+            {
+                var post = (from row in db.BlogPosts where row.ID == ID && !row.IsDeleted select row).FirstOrDefault();
+                if(post != null)
+                {
+                    post.IsDeleted = true;
+                    res = db.SaveChanges();
+                }
+            }
+
+            return res;
+        }
+
+        [HttpPost]
+        [Route("~/Content/SaveBlogPost")]
+        public IActionResult SaveBlogPost(BlogPost request)
+        {
+            using (AgapayTestDBContext db = new AgapayTestDBContext())
+            {
+                var existingPost = (from row in db.BlogPosts where row.ID == request.ID select row).FirstOrDefault();
+                if (existingPost != null)
+                {
+                    existingPost.Title = request.Title;
+                    existingPost.Content = request.Content;
+                    existingPost.BlogStatus = request.BlogStatus;
+                    existingPost.HashSlug = request.HashSlug;
+                    existingPost.ModifiedBy = request.ModifiedBy ?? "SYSTEM";
+                    existingPost.ModifiedDate = DateTime.Now;
+                }
+                else
+                {
+                    request.CreatedBy = request.CreatedBy ?? "SYSTEM";
+                    request.ModifiedBy = request.ModifiedBy ?? "SYSTEM";
+                    request.CreatedDate = DateTime.Now;
+                    request.ModifiedDate = DateTime.Now;
+                    request.IsDeleted = false;
+
+                    db.BlogPosts.Add(request);
+                }
+
+                db.SaveChanges();
+            }
+
+            return Redirect("/Admin/Announcements");
         }
     }
 }
