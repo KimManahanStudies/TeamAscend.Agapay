05196e7fee5e76b3256cc918265f88c9e6fcc0fd
Erwin Mendoza <khronosangel@gmail.com>
2025-10-09 20:54:40 +0800

    [Update] Front end fix, Phonebook CRUD added

--- TeamAscend.Agapay.Web/Controllers/ContactCenterController.cs
+++ TeamAscend.Agapay.Web/Controllers/ContactCenterController.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using TeamAscend.Agapay.Web.Attributes;
 using TeamAscend.Agapay.Web.Models;
 using TeamAscend.Agapay.Web.Shared;
@@ -9,11 +8,84 @@ namespace TeamAscend.Agapay.Web.Controllers
     [PortalAuthorized]
     public class ContactCenterController : Controller
     {
-        
         [Route("~/Admin/Phonebook")]
         public IActionResult Phonebook()
         {
-            return View();
+            var resp = new List<Phonebook>();
+
+            using (AgapayTestDBContext db = new AgapayTestDBContext())
+            {
+                var contacts = (from row in db.Phonebooks where !row.IsDeleted select row).ToList();
+                if (contacts != null)
+                {
+                    resp = contacts;
+                }
+            }
+            return View(resp);
+        }
+
+        [HttpGet]
+        public Phonebook GetContact(int ID)
+        {
+            Phonebook resp = null;
+
+            using (AgapayTestDBContext db = new AgapayTestDBContext())
+            {
+                resp = (from row in db.Phonebooks where row.ID == ID && !row.IsDeleted select row).FirstOrDefault();
+            }
+
+            return resp;
+        }
+
+        [HttpGet]
+        public int DeleteContact(int ID)
+        {
+            int res = 0;
+
+            using (AgapayTestDBContext db = new AgapayTestDBContext())
+            {
+                var contact = (from row in db.Phonebooks where row.ID == ID && !row.IsDeleted select row).FirstOrDefault();
+                if(contact != null)
+                {
+                    contact.IsDeleted = true;
+                    res = db.SaveChanges();
+                }
+            }
+
+            return res;
+        }
+
+        [HttpPost]
+        [Route("~/ContactCenter/SaveContact")]
+        public IActionResult SaveContact(Phonebook request)
+        {
+            using (AgapayTestDBContext db = new AgapayTestDBContext())
+            {
+                var existingContact = (from row in db.Phonebooks where row.ID == request.ID select row).FirstOrDefault();
+                if (existingContact != null)
+                {
+                    existingContact.ContactName = request.ContactName;
+                    existingContact.ContactNo = request.ContactNo;
+                    existingContact.Location = request.Location;
+                    existingContact.BarangayName = request.BarangayName;
+                    existingContact.ModifiedBy = request.ModifiedBy ?? "SYSTEM";
+                    existingContact.ModifiedDate = DateTime.Now;
+                }
+                else
+                {
+                    request.CreatedBy = request.CreatedBy ?? "SYSTEM";
+                    request.ModifiedBy = request.ModifiedBy ?? "SYSTEM";
+                    request.CreatedDate = DateTime.Now;
+                    request.ModifiedDate = DateTime.Now;
+                    request.IsDeleted = false;
+
+                    db.Phonebooks.Add(request);
+                }
+
+                db.SaveChanges();
+            }
+
+            return Redirect("/Admin/Phonebook");
         }
 
         [Route("~/Admin/Hotlines")]
--- TeamAscend.Agapay.Web/Controllers/ProfileController.cs
+++ TeamAscend.Agapay.Web/Controllers/ProfileController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TeamAscend.Agapay.Web.Attributes;
+using TeamAscend.Agapay.Web.Models;
+using TeamAscend.Agapay.Web.Shared;
+using Newtonsoft.Json;
 
 namespace TeamAscend.Agapay.Web.Controllers
 {
@@ -8,7 +11,20 @@ namespace TeamAscend.Agapay.Web.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var currentUser = GetCurrentUser();
+            if (currentUser == null)
+            {
+                return Redirect("/Users/Login");
+            }
+
+            using (AgapayTestDBContext db = new AgapayTestDBContext())
+            {
+                var user = (from row in db.UserAccounts 
+                           where row.ID == currentUser.ID && !row.IsDeleted 
+                           select row).FirstOrDefault();
+                
+                return View(user);
+            }
         }
 
         public IActionResult Settings()
@@ -19,6 +35,78 @@ namespace TeamAscend.Agapay.Web.Controllers
         public IActionResult ActivityLog()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("~/Profile/SaveProfile")]
+        public IActionResult SaveProfile(UserAccount request)
+        {
+            var currentUser = GetCurrentUser();
+            if (currentUser == null || currentUser.ID != request.ID)
+            {
+                return Redirect("/Users/Login");
+            }
+
+            using (AgapayTestDBContext db = new AgapayTestDBContext())
+            {
+                var user = (from row in db.UserAccounts 
+                           where row.ID == request.ID && !row.IsDeleted 
+                           select row).FirstOrDefault();
+                
+                if (user != null)
+                {
+                    user.Username = request.Username;
+                    user.Password = request.Password;
+                    user.FirstName = request.FirstName;
+                    user.LastName = request.LastName;
+                    user.MiddleName = request.MiddleName;
+                    user.EmailAddress = request.EmailAddress;
+                    user.BirthDate = request.BirthDate;
+                    user.ContactNo1 = request.ContactNo1;
+                    user.ContactNo2 = request.ContactNo2;
+                    user.AddressLine1 = request.AddressLine1;
+                    user.AddressLine2 = request.AddressLine2;
+                    user.City = request.City;
+                    user.Region = request.Region;
+                    user.Barangay = request.Barangay;
+                    user.ZipPostCode = request.ZipPostCode;
+                    user.ModifiedBy = user.Username;
+                    user.ModifiedDate = DateTime.Now;
+
+                    db.SaveChanges();
+
+                    // Update session cookie with new user info
+                    UpdateSessionUser(user);
+                }
+            }
+
+            return Redirect("/Profile");
+        }
+
+        private UserAccount GetCurrentUser()
+        {
+            if (Request.Cookies.TryGetValue("AGPSession", out string userJson))
+            {
+                return JsonConvert.DeserializeObject<UserAccount>(userJson);
+            }
+            return null;
+        }
+
+        private void UpdateSessionUser(UserAccount user)
+        {
+            if (Request.Cookies.Keys.Contains("AGPSession"))
+            {
+                Response.Cookies.Delete("AGPSession");
+            }
+            
+            CookieOptions option = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(2),
+                Secure = true
+            };
+            
+            string userJson = JsonConvert.SerializeObject(user);
+            Response.Cookies.Append("AGPSession", userJson, option);
         }
     }
 }
